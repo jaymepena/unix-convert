b89745947bfe4a0a63d7f60701ae0dc97514b02a
Jayme Pena <99447391+jaymepena@users.noreply.github.com>
2023-07-05 16:20:08 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace unix_convert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonConvertUnixToDateTime_Click(object sender, RoutedEventArgs e)
+        {
+            var input = long.Parse(txtUnixInput.Text);
+
+            if (input >= Decimal.Parse("1E11", System.Globalization.NumberStyles.Float))
+            {
+                DateTimeOffset offset = DateTimeOffset.FromUnixTimeMilliseconds(input);
+                output.Items.Clear();
+                output.Items.Add(offset);
+            }
+            else
+            {
+                DateTimeOffset offset = DateTimeOffset.FromUnixTimeSeconds(input);
+                output.Items.Clear();
+                output.Items.Add(offset);
+            }
+
+            output.Items.Add(txtUnixInput.Text);
+
+            txtUnixInput.Clear();
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            output.Items.Clear();
+            txtUnixInput.Clear();
+        }
+    }
+}
